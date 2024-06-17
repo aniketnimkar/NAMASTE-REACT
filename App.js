23b4f66d002a6ba84a2a2e0962966c62a768cac2
aniketnimkar <aniketnimkar88@gmail.com>
2024-06-17 17:09:32 +0530

    EP-3

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is aniket 💖"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+//jsx - HTML Like or XML-Like syntax
 
-console.log(parent);
+//JSX (Transpiled before it reached to the JS) - PARCEL - Babel(babel do this transpilation which is provided by Parcel)
+// const jsxHeading = <h1 id="heading">Namaste React using JSX💖</h1>;
+// console.log(jsxHeading);
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX🚀
+  </h1>
+);
+
+const number = 10000;
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    {number}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-// start from 41 min
+root.render(<HeadingComponent />);
